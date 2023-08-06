84e0a5683455f9e710e2fe01521db42f8300fd68
Alexander Kosyakov <110721351+gOweRkrd@users.noreply.github.com>
2023-08-06 10:32:02 +0300

    Add files via upload

---
+++ Encyclopedia of cats/ViewController.swift
@@ -0,0 +1,15 @@
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        }
+    }
+}
+
